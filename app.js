cf2725aa0a98dd5e8d0ccc6320556ba132d6c01e
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2021-07-02 15:03:34 +0900

    3.7 css in javascript study

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello h1");
 
 function handelTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    } else{
-        newColor = "blue";
+    const clickedClass = "clicked";
+    if(h1.className === clickedClass){
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handelTitleClick);
