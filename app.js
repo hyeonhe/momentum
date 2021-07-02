23df2cd3d3f588c2e8737588d8883ef561ce870a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2021-07-02 13:58:37 +0900

    3.6 css in javascript study

--- app.js
+++ app.js
@@ -1,42 +1,14 @@
 const h1 = document.querySelector(".hello h1");
 
-console.dir(h1);
-
-function handleh1Click(){
-    console.log("h1 was clicked!");
+function handelTitleClick(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else{
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    console.log("mouse is here");
-}
-
-function handleMouseLeave(){
-    console.log("mouse was leaved");
-}
-
-function handleWindowResize(){
-    document.body.style.background = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-
-// h1.onclick = handleh1Click;
-h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handelTitleClick);
