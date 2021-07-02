0cfb574c5843cac579106a3f91d5a2b8e7d1395d
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2021-07-02 17:25:31 +0900

    3.8 css in javascript three study

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const h1 = document.querySelector(".hello h1");
 
 function handelTitleClick(){
-    const clickedClass = "clicked";
-    if(h1.className === clickedClass){
-        h1.className = "";
-    } else {
-        h1.className = clickedClass;
-    }
+    h1.classList.toggle("clicked");
+    // const clickedClass = "clicked";
+    // if(h1.classList.contains(clickedClass)){
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
 }
 
 h1.addEventListener("click", handelTitleClick);
