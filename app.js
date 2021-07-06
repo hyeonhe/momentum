21154e65deb1639d0d66c40366aa4a1586d40e6a
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2021-07-06 12:08:39 +0900

    4.2 Events

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-// // const loginForm = document.getElementById("login-form");
-// const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
-
-// function onLoginBtnClick() {
-//     console.log("clicksdfsad");
-// }
-
-// loginButton.addEventListener("click", onLoginBtnClick);
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    // if(username === ""){
-    //     alert("Please write your name");
-    // } else if(username.length > 15){
-    //     alert("Your name is too long.");
-    // } else{
-
-    // }
-    console.log(username);
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
